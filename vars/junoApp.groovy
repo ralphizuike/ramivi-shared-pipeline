a6b8ce3b5f5c644a92e9a71c5219fb8190ebfc58
ralphizuike <106279864+ralphizuike@users.noreply.github.com>
2022-06-12 00:30:42 -0500

    Update junoApp.groovy

--- vars/junoApp.groovy
+++ vars/junoApp.groovy
@@ -1,27 +1,68 @@
 def call(String repoUrl){
-  pipeline {
-       agent any
-       tools {
-           maven 'maven'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh 'mvn --version'
-                   sh 'java -version'
-               }
-           }
-           stage("Checkout Code") {
+  pipeline{
+     	agent any
+     	stages{
+     		stage("Checkout Code") {
                steps {
                    git branch: 'main',
                           url: "${repoUrl}"
                }
            }
-           stage("to-test-maven") {
-               steps {
-                   sh 'mvn -v'
-               }
-           }
-       }
-}
+     		stage('parallel-job'){
+     			parallel{
+     				stage('sub-job1'){
+     					steps{
+     						echo 'action1'
+     					}
+     				}
+     				stage('sub-job2'){
+     					steps{
+     						sh 'systemctl status jenkins'
+     					}
+     				}
+     				stage('sub-job3'){
+     					steps{
+     						sh 'ps -ef'
+
+     					}
+     				}
+     				stage('sub-job4'){
+     					steps{
+     						echo 'Dr Eng Ike'
+     					}
+     					
+     				}
+     			}
+
+     		}
+     		stage('codebuild'){
+     			steps{
+     				sh 'cat /etc/passwd'
+     			}
+     		}
+             stage('parallel-job2'){
+                 parallel{
+                     stage('check'){
+                         steps{
+                             sh 'ps -ef'
+                         }
+                     }
+                     stage('calander'){
+                         steps{
+                             sh 'cal 2025'
+                         }
+                     }
+                     stage('lscpu'){
+                         steps{
+                             sh 'lscpu'
+                         }
+                     }
+                      stage('jenkinsID'){
+                           steps{
+                             sh 'ls'
+                            }
+                 }
+             }
+     	}
+     }
 }
